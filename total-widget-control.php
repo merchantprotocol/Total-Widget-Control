80f8ebbc6ad58dc05af975509c89fefd57021cfc
byrd <byrd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-20 19:56:57 +0000

    bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/total-widget-control/trunk@362623 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- total-widget-control.php
+++ total-widget-control.php
@@ -255,22 +255,6 @@ function twc_clear_license( $inside = false )
 }
 
 /**
- * Function is responsible for clearing the current license
- *
- * @return null
- */
-function twc_clear_originals( $inside = false )
-{
-	if (!$inside && !array_key_exists('twc_clear_originals', $_REQUEST)) return false;
-	if (TWC_CURRENT_USER_CANNOT) return false;
-	
-	update_option('twc_first_activate',array());
-	
-	wp_redirect( admin_url('widgets.php') );
-	exit();
-}
-
-/**
  * function inserts the contextual help
  * This inserts a contextual expanding area.
  *
@@ -2013,7 +1997,7 @@ function twc_set_object_id()
 		wp_reset_query();
 	}
 	
-	if (!$wp_query->have_posts())
+	if (!$wp_query->have_posts() && is_admin())
 	{
 		//this is used for the admin area
 		if (isset($_REQUEST['post']) && $post = get_post($_REQUEST['post']))
